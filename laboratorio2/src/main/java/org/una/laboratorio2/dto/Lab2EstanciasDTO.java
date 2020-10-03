1e1e3ad391336a340ca8f435aa4570a1d95bec21
Bosco <bosco11jico100@gmail.com>
2020-10-03 14:59:19 -0600

    estanciasDTO

--- laboratorio2/src/main/java/org/una/laboratorio2/dto/Lab2EstanciasDTO.java
+++ laboratorio2/src/main/java/org/una/laboratorio2/dto/Lab2EstanciasDTO.java
@@ -5,10 +5,31 @@
  */
 package org.una.laboratorio2.dto;
 
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  *
  * @author colo7
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Lab2EstanciasDTO {
+    
+    private Long id;
+    
+    private Date fechaRegistro;
+    
+    private Lab2IntervencionesDTO intervencionesId;
+    
+    private Lab2PacientesDTO pacienteId;
+    
+    private Lab2UnidadesDTO unidadId;
+   
     
 }
