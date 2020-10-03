5d23e4bc2947f743df66895bd6219482b93727b8
Gus2730 <gus2730@gmail.com>
2020-10-02 21:41:23 -0600

    correcciones en los services implementation

--- laboratorio2/src/main/java/org/una/laboratorio2/service/Lab2DoctoresServiceImplementation.java
+++ laboratorio2/src/main/java/org/una/laboratorio2/service/Lab2DoctoresServiceImplementation.java
@@ -7,7 +7,7 @@ package org.una.laboratorio2.service;
 
 import java.util.List;
 import java.util.Optional;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.una.laboratorio2.dto.Lab2DoctoresDTO;
--- laboratorio2/src/main/java/org/una/laboratorio2/service/Lab2UnidadesServiceImplementation.java
+++ laboratorio2/src/main/java/org/una/laboratorio2/service/Lab2UnidadesServiceImplementation.java
@@ -7,7 +7,6 @@ package org.una.laboratorio2.service;
 
 import java.util.List;
 import java.util.Optional;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.una.laboratorio2.dto.Lab2UnidadesDTO;
@@ -16,6 +15,7 @@ import org.una.laboratorio2.entities.Lab2Unidades;
 import org.una.laboratorio2.repositories.ILab2UnidadesRepository;
 import org.una.laboratorio2.utils.ConversionLista;
 import org.una.laboratorio2.utils.MapperUtils;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
