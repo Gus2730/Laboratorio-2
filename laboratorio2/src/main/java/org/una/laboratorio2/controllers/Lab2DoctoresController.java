ceae74b25be19526f1e7f3009c9ab8e0334796a2
Gus2730 <gus2730@gmail.com>
2020-10-02 22:44:18 -0600

    cambios

--- laboratorio2/src/main/java/org/una/laboratorio2/controllers/Lab2DoctoresController.java
+++ laboratorio2/src/main/java/org/una/laboratorio2/controllers/Lab2DoctoresController.java
@@ -8,9 +8,11 @@ package org.una.laboratorio2.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.una.laboratorio2.service.ILab2DoctoresService;
@@ -19,6 +21,7 @@ import org.una.laboratorio2.service.ILab2DoctoresService;
  *
  * @author Bosco
  */
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
 @RequestMapping("/doctores")
 public class Lab2DoctoresController {
