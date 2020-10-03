2b1fe0517517cfab131a8fadf2d44da0a35e1aff
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-10-02 21:35:10 -0600

    Create ILab2EstanciasRepository.java

---
+++ laboratorio2/src/main/java/org/una/laboratorio2/repositories/ILab2EstanciasRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.laboratorio2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.una.laboratorio2.entities.Lab2Estancias;
+
+/**
+ *
+ * @author colo7
+ */
+public interface ILab2EstanciasRepository extends JpaRepository<Lab2Estancias, Long>{
+    
+}
