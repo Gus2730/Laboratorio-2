1dd15a8277b0c8ccc1d5de42155acb083361a21f
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-10-02 21:57:48 -0600

    add etiquetas

--- laboratorio2/src/main/java/org/una/laboratorio2/controllers/Lab2DoctoresController.java
+++ laboratorio2/src/main/java/org/una/laboratorio2/controllers/Lab2DoctoresController.java
@@ -8,18 +8,25 @@ package org.una.laboratorio2.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.una.laboratorio2.service.ILab2DoctoresService;
 
 /**
  *
  * @author Bosco
  */
+@RestController
+@RequestMapping("/doctores")
 public class Lab2DoctoresController {
+    
     @Autowired
     private ILab2DoctoresService ILab2DoctoresService;
-   
+    
+    @GetMapping()
     public @ResponseBody
     ResponseEntity<?> findAll() {
         try {
@@ -29,7 +36,7 @@ public class Lab2DoctoresController {
             return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
+    @GetMapping("/id")
     public ResponseEntity<?> findById(@PathVariable(value = "id") Long id) {
         try {
             return new ResponseEntity(ILab2DoctoresService.findById(id), HttpStatus.OK);
